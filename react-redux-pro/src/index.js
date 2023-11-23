75ca9d4e70b5d3eb32e0a777bfab7d67fb8092ac
3358115621@qq.com <3358115621@qq.com>
2023-11-23 20:16:57 +0800

    init redux

--- react-redux-pro/src/index.js
+++ react-redux-pro/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import counterReducer from './modules/counterStore'
+import {configureStore} from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
+
+export default store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
