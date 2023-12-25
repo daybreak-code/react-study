de2a8cbc9e0b68f98418ca3218a372aebcc8522d
3358115621@qq.com <3358115621@qq.com>
2023-12-25 22:11:20 +0800

    init Layout and Month

--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -1,8 +1,9 @@
-import { TabBar } from "antd-mobile"
-import { useEffect } from "react"
-import { Outlet, useNavigate } from "react-router-dom"
-import { useDispatch } from 'react-redux'
-import { getBillList } from "@/store/modules/billStore"
+import { TabBar } from "antd-mobile";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getBillList } from "@/store/modules/billStore";
+
 import './index.scss'
 import {
   BillOutline,
@@ -10,46 +11,48 @@ import {
   AddCircleOutline
 } from 'antd-mobile-icons'
 
+
 const tabs = [
   {
     key: '/month',
     title: '月度账单',
-    icon: <BillOutline />,
+    icon: <BillOutline />
   },
   {
     key: '/new',
     title: '记账',
-    icon: <AddCircleOutline />,
+    icon: <AddCircleOutline />
   },
   {
     key: '/year',
     title: '年度账单',
-    icon: <CalculatorOutline />,
-  },
+    icon: <CalculatorOutline />
+  }
 ]
 
 const Layout = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(getBillList())
-  }, [dispatch])
-
-  // 切换菜单跳转路由
   const navigate = useNavigate()
+
   const swithRoute = (path) => {
     console.log(path)
     navigate(path)
   }
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getBillList())
+  }, [dispatch])
   return (
     <div className="layout">
       <div className="container">
         <Outlet />
       </div>
       <div className="footer">
-        <TabBar onChange={swithRoute}>
-          {tabs.map(item => (
-            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
-          ))}
+        <TabBar onChange={swithRoute('/test')}>
+          {
+            tabs.map(item => (
+              <TabBar.Item key={item.key} icon={item.icon} title={item.title}></TabBar.Item>
+            ))
+          }
         </TabBar>
       </div>
     </div>
--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,81 +1,44 @@
-import { NavBar, DatePicker } from 'antd-mobile'
-import { useEffect, useMemo, useState } from 'react'
+import { NavBar, DatePicker } from "antd-mobile";
 import './index.scss'
-import classNames from 'classnames'
-import dayjs from 'dayjs'
-import { useSelector } from 'react-redux'
 
 const Month = () => {
-  // 按月做数据的分组
-  const billList = useSelector(state => state.bill.billList)
-  // 控制弹框的打开和关闭
-  const monthGroup = useMemo(() => {
-    return billList
-  }, [billList])
-
-  useEffect(() => {
-    const nowDate = new Date();
-    dayjs(nowDate).format('YYYY-MM')
-  }, [])
-
-  console.log(monthGroup)
-  const [currentMonthList, setMonthList] = useState([])
-  
-  const [dateVisiable, setDateVisible] = useState(false)
-  const [currentDate, setCurrentDate] = useState(() => {
-    return dayjs(new Date()).format('YYYY-MM')
-  })
-  const onConfirm = (date) => {
-    setDateVisible(false)
-    //console.log(date)
-    const formatDate = dayjs(date).format('YYYY-MM')
-    setMonthList(monthGroup[formatDate])
-    setCurrentDate(formatDate)
-  }
   return (
-    <div className="monthlyBill">
+    <div className="monthliBill">
       <NavBar className="nav" backArrow={false}>
         月度收支
       </NavBar>
       <div className="content">
         <div className="header">
-          {/* 时间切换区域 */}
-          <div className="date" onClickCapture={() => setDateVisible(true)}>
+          <div className="date"> 
             <span className="text">
-              {currentDate + ''} | 3月账单
+              2023 | 3 月账单
             </span>
-            {/*根据当前弹框打开的状态控制 expand 类名是否存在 */}
-            <span className={classNames('arrow', dateVisiable && 'expand')}></span>
+            <span className="arrow expand"></span>
           </div>
-          {/* 统计区域 */}
-          <div className='twoLineOverview'>
+          <div className="twoLineOverview">
             <div className="item">
               <span className="money">{100}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{100}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{100}</span>
               <span className="type">结余</span>
             </div>
           </div>
-          {/* 时间选择器 */}
-          <DatePicker
-            className="kaDate"
-            title="记账日期"
-            precision="month"
-            visible={dateVisiable}
-            onCancel={() => setDateVisible(false)}
-            onConfirm={onConfirm}
-            onClose={() => setDateVisible(false)}
-            max={new Date()}
-          />
         </div>
+        <DatePicker
+          className="kaDate"
+          title="记账日期"
+          precision="month"
+          visible={false}
+          max={new Date()}
+        />
       </div>
-    </div >
+    </div>
   )
 }
 
