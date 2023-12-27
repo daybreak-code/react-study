0ebd5f56bb7835355aa216eb59e3ad4569e86a44
3358115621@qq.com <3358115621@qq.com>
2023-12-27 08:11:11 +0800

    init month function

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,17 +1,54 @@
+import { useSelector } from "react-redux"
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
-import {  useState } from "react"
+import _ from 'lodash'
+import dayjs from "dayjs"
+import { useMemo, useState } from "react"
+import { useEffect } from "react"
 import classNames from "classnames"
 
 const Month = () => {
+  // 按月分组
+  const billList = useSelector(state => state.bill.billList)
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList, item => dayjs(item.date).format('YYYY-MM'))
+  }, [billList])
 
+  // 控制时间选择器打开关闭
   const [dateVisible, setDateVisible] = useState(false)
+  const [currentMonthList, setMonthList] = useState([])
+  const [currentMonth, setCurrentMonth] = useState(() => {
+    return dayjs().format('YYYY-MM')
+  })
 
-  // 时间选择框确实事件
   const dateConfirm = (date) => {
-    // 关闭弹框
     setDateVisible(false)
+    const monthKey = dayjs(date).format('YYYY-MM')
+    setCurrentMonth(monthKey)
+    setMonthList(monthGroup[monthKey])
   }
+
+  // 首次加载
+  useEffect(() => {
+    const list = monthGroup[dayjs().format('YYYY-MM')]
+    if(list){
+      setMonthList(list)
+    }
+  }, [monthGroup])
+
+  // 计算统计
+  const overview = useMemo(() => {
+    if (!currentMonthList) return { income: 0, pay: 0, total: 0 }
+    const income = currentMonthList.filter(item => item.type === 'income')
+      .reduce((a, c) => a + c.money, 0)
+    const pay = currentMonthList.filter(item => item.type === 'pay')
+      .reduce((a, c) => a + c.money, 0)
+    return {
+      income,
+      pay,
+      total: income + pay
+    }
+  }, [currentMonthList])
 
   return (
     <div className="monthlyBill">
@@ -22,23 +59,23 @@ const Month = () => {
         <div className="header">
           {/* 时间切换区域 */}
           <div className="date" onClick={() => setDateVisible(true)}>
-            <span className='text'>
-              2023 | 3月账单
+            <span className="text">
+              {currentMonth} 账单
             </span>
             <span className={classNames('arrow', dateVisible && 'expand')}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{overview.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{overview.income.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{(overview.total).toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
@@ -46,6 +83,7 @@ const Month = () => {
           <DatePicker
             className="kaDate"
             title="记账日期"
+            precision="month"
             visible={dateVisible}
             max={new Date()}
             onConfirm={dateConfirm}
