b8e25e2f088530c9ee8e642823a0aa4155e02b6c
3358115621@qq.com <3358115621@qq.com>
2024-01-07 20:55:23 +0800

    init login function

--- react-blog/src/router/index.js
+++ react-blog/src/router/index.js
@@ -1,9 +1,9 @@
-import { createBrowerRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom';  
 
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
 
-const router = createBrowerRouter([
+const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />
