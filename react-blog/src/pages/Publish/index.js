d71c1c9443b16cac4a329b680e69bc770270744a
3358115621@qq.com <3358115621@qq.com>
2024-01-12 21:06:17 +0800

    init data

--- react-blog/src/pages/Publish/index.js
+++ react-blog/src/pages/Publish/index.js
@@ -24,8 +24,11 @@ const { Option } = Select
 const Publish = () => {
 
   const [searchParams] = useSearchParams()
+
   const articleId = searchParams.get('id')
+
   const [form] = Form.useForm()
+
   useEffect(() => {
     async function getArticle(){
       const res = await http.get(`/mp/article/${articleId}`)
@@ -50,7 +53,6 @@ const Publish = () => {
   }, [])
 
   const onFinish = async (formValue) => {
-    if (imageType != imageList.length) return message.warning('图片类型和数量不一致')
     const { channel_id, content, title} = formValue
     const formatUrl = (list) => {
       return list.map(item => {
@@ -144,36 +146,41 @@ const Publish = () => {
               ))}
             </Select>
           </Form.Item>
+
           <Form.Item
             label="内容"
             name="content"
             rules={[{ required: true, message: '请输入文章内容' }]}
-          ></Form.Item>
-          <Form.Item label="封面">
-          <Form.Item name="type">
-            <Radio.Group>
-              <Radio value={1}>单图</Radio>
-              <Radio value={3}>三图</Radio>
-              <Radio value={0}>无图</Radio>
-            </Radio.Group>
+          >
+
+            <Input placeholder="Basic usage" />
+            
           </Form.Item>
-           {imageType > 0 && 
-              <Upload
-                  name="image"
-                  listType="picture-card"
-                  showUploadList
-                  action={'http://geek.itheima.net/v1_0/upload'}
-                  onChange={onUploadChange}
-                  maxCount={imageType}
-                  multiple={imageType > 1}
-                  fileList={imageList}
-              >
-                <div style={{ marginTop: 8 }}>
-                  <PlusOutlined />
-                </div>
-              </Upload>
-            }
-        </Form.Item>
+          <Form.Item label="封面">
+            <Form.Item name="type" onTypeChange={onTypeChange}>
+              <Radio.Group onRadioChange={onRadioChange}>
+                <Radio value={1}>单图</Radio>
+                <Radio value={3}>三图</Radio>
+                <Radio value={0}>无图</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {imageType > 0 && 
+                <Upload
+                    name="image"
+                    listType="picture-card"
+                    showUploadList
+                    action={'http://geek.itheima.net/v1_0/upload'}
+                    onChange={onUploadChange}
+                    maxCount={imageType}
+                    multiple={imageType > 1}
+                    fileList={imageList}
+                >
+                  <div style={{ marginTop: 8 }}>
+                    <PlusOutlined />
+                  </div>
+                </Upload>
+              }
+          </Form.Item>
 
           <ReactQuill 
             className='publish-quill' 
