ddd665e4b6e73d7ba5ed9eefad78158e68820686
3358115621@qq.com <3358115621@qq.com>
2024-07-11 22:54:45 +0800

    33. 登陆页面-验证码模块布局

--- react-management/src/views/Login/index.tsx
+++ react-management/src/views/Login/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 import styles from "./login.module.css"
 import initLoginBtn from './init.ts'
+import { Input, Space, Button } from 'antd';
+import './login.less'
 
 const View = () => {
 
@@ -16,10 +18,25 @@ const View = () => {
             {/* 存放背景 */}
             <canvas id="canvas" style={{display:"block"}}></canvas>
             {/* 登录盒子 */}
-            <div className={styles.loginBox}>
+            <div className={styles.loginBox + " loginbox"}>
+                {/* 标题部分 */}
                 <div className={styles.title}>
                     <h1>前端乐哥 &nbsp;&nbsp; 通用后台系统</h1>
                     <p>Strive Everyday</p>
+                </div>
+                {/* 表单部分 */}
+                <div className="form">
+                    <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                        <Input placeholder="Basic usage" />
+                        <Input.Password placeholder="input password" />
+                        <div className="captchaBox">
+                            <Input placeholder="Verify code" />
+                            <div className="captchaImg">
+                                <img height="38" src="./verify_code.png" alt="./verify_code.png" />
+                            </div>
+                        </div>
+                        <Button type="primary" className="loginBtn">登录</Button>
+                    </Space>
                 </div>
             </div>
         </div>
