8af1f82694bb6767e54f40b91525dd780c54cd03
3358115621@qq.com <3358115621@qq.com>
2024-07-12 22:08:39 +0800

    43. 创建仓库并和数据关联上

--- react-management/src/main.tsx
+++ react-management/src/main.tsx
@@ -9,10 +9,14 @@ import "./assets/styles/global.scss"
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+//状态管理的库
+import {Provider} from "react-redux"
+import store from './store/index.ts'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+        <App />
+      </BrowserRouter>
+  </Provider>
 )
---
+++ react-management/src/store/index.ts
@@ -0,0 +1,7 @@
+import {legacy_createStore} from "redux"
+import reducer from "./reducer.ts"
+
+//创建数据仓库
+const store = legacy_createStore(reducer)
+
+export default store
