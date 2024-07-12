681b889594a00967314234ac970e255f82c2643c
3358115621@qq.com <3358115621@qq.com>
2024-07-12 22:51:26 +0800

    45. 在组件中修改仓库数据

--- react-management/src/store/reducer.ts
+++ react-management/src/store/reducer.ts
@@ -1,8 +1,19 @@
 const defaultState = {
     num: 20
 }
-let reducer = (state = defaultState,) => {
+let reducer = (state = defaultState, action: {type: string}) => {
     let newState = JSON.parse(JSON.stringify(state))
+    switch (action.type){
+        case 'add1':
+            newState.num++
+            break;
+        case 'add2':
+            newState.num += 2
+            break;
+        default:
+            break;
+    }
+
     return newState
 }
 
--- react-management/src/views/Page1.tsx
+++ react-management/src/views/Page1.tsx
@@ -1,7 +1,22 @@
+import { useSelector,useDispatch } from "react-redux"
+
 const View = () => {
+    // 获取仓库数据
+    const {num} = useSelector((state) => ({
+        num: state.num
+    }))
+
+    const dispatch = useDispatch();
+
+    const changeNum = () => {
+        dispatch({type: 'add1'})
+    }
+
     return (
         <div className='home'>
             <p>这是Page1页面</p>
+            <p>{num}</p>
+            <button onClick={changeNum}>点击按钮</button>
         </div>
     )
 }
