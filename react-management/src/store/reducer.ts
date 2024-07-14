350efaaab37126af695b5ea887139a89fe0c2e3b
3358115621@qq.com <3358115621@qq.com>
2024-07-14 11:06:10 +0800

    47. 数据和方法从reducer中抽离

--- react-management/src/store/reducer.ts
+++ react-management/src/store/reducer.ts
@@ -1,14 +1,17 @@
+import handleNum from "./NumStatus"
+
 const defaultState = {
-    num: 20
+    //num:handleNum.state.num //这种数据一多要写很多次
+    ...handleNum.state  // 解构的写法
 }
-let reducer = (state = defaultState, action: {type: string}) => {
+let reducer = (state = defaultState, action: {type: string, val: number}) => {
     let newState = JSON.parse(JSON.stringify(state))
     switch (action.type){
         case 'add1':
-            newState.num++
+            handleNum.actions.add1(newState, action)
             break;
         case 'add2':
-            newState.num += 2
+            handleNum.actions.add2(newState, action)
             break;
         default:
             break;
