d71c9ea56b4c0722c3320016467b78417d610922
3358115621@qq.com <3358115621@qq.com>
2024-07-15 07:37:36 +0800

    63. 数据交互-登录的业务逻辑处理

--- react-management/src/request/api.ts
+++ react-management/src/request/api.ts
@@ -1,6 +1,8 @@
 import request from "./index.ts"
 
 //验证码请求
-export const CaptchaAPI = () => {
+export const CaptchaAPI = ():Promise<CaptchaAPIRes> => {
     return request.get("/prod-api/captchaImage")
 }
+
+export const LoginAPI = (params:LoginAPIReq):Promise<LoginAPIRes> =>request.post("/prod-api/login",params);
--- react-management/src/views/Login/index.tsx
+++ react-management/src/views/Login/index.tsx
@@ -1,11 +1,14 @@
 import { ChangeEvent, useEffect, useState } from "react"
 import styles from "./login.module.css"
 import initLoginBtn from './init.ts'
-import { Input, Space, Button } from 'antd';
+import { Input, Space, Button, message } from 'antd';
 import './login.less'
-import {CaptchaAPI} from "../../request/api.ts"
+import {CaptchaAPI, LoginAPI} from "../../request/api.ts"
+import {useNavigate} from "react-router-dom"
 
 const View = () => {
+
+    let navigateTo = useNavigate();
 
     useEffect(() => {
         initLoginBtn();
@@ -18,6 +21,7 @@ const View = () => {
     const [userName, setUserName] = useState('');
     const [passwordVal, setPasswordVal] = useState('');
     const [captchaVal, setCaptchaVal] = useState('');
+    const [captchaImg, setCaptchaImg] = useState('');
 
     const changeUserName = (e: ChangeEvent<HTMLInputElement>) => {
         setUserName(e.target.value)
@@ -34,14 +38,42 @@ const View = () => {
         console.log(captchaVal)
     }
 
-    const loginBtn = () => {
-       console.log("username is: password is: captcha is: ", userName, passwordVal, captchaVal)
-    }
-
     const getCaptchaImage = async () => {
         let captchaAPIRes = await CaptchaAPI();
         console.log(captchaAPIRes);
+        if (captchaAPIRes.code === 200){
+            setCaptchaImg("data:image/gif;base64," + captchaAPIRes.img)
+            localStorage.setItem("uuid",captchaAPIRes.uuid)
+        }
+    }
 
+    const gotoLogin = async () => {
+        console.log("username and password and verify code is: ", userName, passwordVal, captchaVal)
+        if (!userName.trim() || !passwordVal.trim() || !captchaVal.trim()){
+            message.warning("Pls input information completely")
+            return
+        }
+        let loginAPIRes = await LoginAPI({
+            username:userName,
+            password:passwordVal,
+            code:captchaVal,   
+            uuid:localStorage.getItem("uuid") as string    
+          })
+
+          console.log(loginAPIRes);
+
+          if(loginAPIRes.code===200){
+            // 1、提示登录成功
+            message.success("登录成功！")
+            // 2、保存token
+            localStorage.setItem("lege-react-management-token",loginAPIRes.token)
+            // 3、跳转到/page1
+            navigateTo("/page1")
+            // 4、删除本地保存中的uuid
+            localStorage.removeItem("uuid")
+          }
+      
+      
     }
 
     return (
@@ -63,10 +95,10 @@ const View = () => {
                         <div className="captchaBox">
                             <Input placeholder="Verify code" onChange={changeCaptcha} />
                             <div className="captchaImg" onClick={getCaptchaImage}>
-                                <img height="38" src="./verify_code.png" alt="./verify_code.png" />
+                                <img height="38" src={captchaImg} alt="" />
                             </div>
                         </div>
-                        <Button type="primary" className="loginBtn" onClick={loginBtn}>登录</Button>
+                        <Button type="primary" className="loginBtn" onClick={gotoLogin}>登录</Button>
                     </Space>
                 </div>
             </div>
