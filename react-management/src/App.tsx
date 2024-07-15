bcc31ea665173493d1637d1899ac61fdbb63e63d
3358115621@qq.com <3358115621@qq.com>
2024-07-15 08:04:00 +0800

    65. 添加前置路由守卫

--- react-management/src/App.tsx
+++ react-management/src/App.tsx
@@ -3,10 +3,46 @@
 // import Comp2 from './components/Comp2'
 // import { Button } from 'antd';
 // import {CaretDownOutlined} from '@ant-design/icons'
-import { useRoutes, Link } from 'react-router-dom';
+import { useRoutes, useLocation, useNavigate } from 'react-router-dom';
 import routes from "./router"
+import { useEffect } from 'react';
+import { message } from 'antd';
 
 function App() {
+
+  function ToLogin(){
+    const navigateTo = useNavigate()
+    useEffect(() => {
+      navigateTo("/login");
+      message.warning("You don't login before, pls login first then access")
+    }, [])
+    return <div></div>
+  }
+
+  
+  function ToPage1(){
+    const navigateTo = useNavigate()
+    useEffect(() => {
+      navigateTo("/page1");
+      message.warning("You already logged before")
+    }, [])
+    return <div></div>
+  }
+
+  function BeforeRouterEnter(){
+    const outlet = useRoutes(routes)
+    const location = useLocation();
+    let token = localStorage.getItem("lege-react-managent-token");
+    //1. 如果访问的是登录页面，并且有token，跳转到首页
+    if (location.pathname === "/login" && token){
+      return <ToPage1 />
+    }
+    //2. 如果访问的不是登录页面，并且没有token，跳转到登录页
+    if (location.pathname !== "/login" && !token){
+      return <ToLogin />
+    }
+    return outlet
+  }
 
   // const [count, setCount] = useState(0)
   const outlet = useRoutes(routes)
@@ -23,7 +59,7 @@ function App() {
 
       {/* 占位符组件，类似于窗口，用来展示组件 */}
       {/* <Outlet></Outlet> */}
-      {outlet}
+      <BeforeRouterEnter />
     </div>
   )
 }
